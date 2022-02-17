3b471742eb07df106fde6e733c145d22867e2239
Vincenzo Grieco <80895148+VincenzoGUNISA@users.noreply.github.com>
2022-02-17 17:05:35 +0100

    Added new script to change the backgroun image, with different size.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function changeText(){
-    document.body.style.backgroundImage="url('https://thumbs.dreamstime.com/z/pino-31293091.jpg')";
+    document.body.style.backgroundImage="url('images/pino-31293091.jpg')";
+}
+
+function changeTextAgain (){
+    document.body.style.backgroundImage="url('images/franksinatra.jpg')";
+    document.body.style.backgroundRepeat="no-repeat";
+    document.body.style.backgroundSize="cover";
 }
